b046b811f2182a8833085f9d51ee691bff220073
Nazar <fominovnazar@redcollar.ru>
2025-07-23 23:05:25 +0300

    update registry component

--- registry/App.tsx
+++ registry/App.tsx
@@ -1,8 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { DateBlock } from "./components/DateBlock";
+import {DateBlock} from "./components/DateBlock";
 
 function App() {
   return (
